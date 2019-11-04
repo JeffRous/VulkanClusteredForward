dbd4ae6c8ed42d9d48673518f03e314f18ee4ada
starcid <xing.cidhx@gmail.com>
2019-11-04 15:50:51 +0800

    add new compute shader

--- Source/Shader/compile_shader.bat
+++ Source/Shader/compile_shader.bat
@@ -3,4 +3,5 @@ C:\VulkanSDK\1.1.121.2\Bin\glslc sample.frag -o ../../Data/shader/sample_frag.sp
 C:\VulkanSDK\1.1.121.2\Bin\glslc tinyobj.vert -o ../../Data/shader/tinyobj_vert.spv
 C:\VulkanSDK\1.1.121.2\Bin\glslc tinyobj.frag -o ../../Data/shader/tinyobj_frag.spv
 C:\VulkanSDK\1.1.121.2\Bin\glslc cluste_calc.comp -o ../../Data/shader/cluste_calc.spv
+C:\VulkanSDK\1.1.121.2\Bin\glslc cluste_culling.comp -o ../../Data/shader/cluste_culling.spv
 pause
